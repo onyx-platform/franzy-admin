f4a48ecac04d1a2e4f3852903e494b7480b28db4
Michael Drogalis <mjd3089@rit.edu>
2017-02-16 12:31:00 -0800

    Ditch repositories key.

--- project.clj
+++ project.clj
@@ -4,14 +4,6 @@
             :author "ymilky"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-            :repositories {"snapshots" {:url           "https://clojars.org/repo"
-                                        :username      :env
-                                        :password      :env
-                                        :sign-releases false}
-                           "releases"  {:url           "https://clojars.org/repo"
-                                        :username      :env
-                                        :password      :env
-                                        :sign-releases false}}
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [prismatic/schema "1.1.0"]
                            [org.apache.kafka/kafka_2.11 "0.10.0.1" :exclusions [org.scala-lang/scala-library]]
