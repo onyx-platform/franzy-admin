f640f6183cce9e4255d2da1c094b4587b38b57f2
Michael Drogalis <mjd3089@rit.edu>
2017-02-16 12:28:30 -0800

    Bump for Kafka 0.10.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ymilky/franzy-admin "0.0.2-SNAPSHOT"
+(defproject org.onyxplatform/franzy-admin "0.0.3"
             :description "A Kafka Clojure Admin client, with support for topic, partition, group, cluster management, and more."
             :url "https://github.com/ymilky/franzy-admin"
             :author "ymilky"
@@ -14,9 +14,9 @@
                                         :sign-releases false}}
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [prismatic/schema "1.1.0"]
-                           [org.apache.kafka/kafka_2.11 "0.9.0.1" :exclusions [org.scala-lang/scala-library]]
+                           [org.apache.kafka/kafka_2.11 "0.10.0.1" :exclusions [org.scala-lang/scala-library]]
                            [org.scala-lang/scala-library "2.11.8"]
-                           [ymilky/franzy-common "0.0.2-SNAPSHOT"]
+                           [lbradstreet/franzy-common "0.0.2"]
                            [com.taoensso/timbre "4.3.1"]]
             :plugins [[lein-codox "0.9.4"]]
             :codox {:metadata    {:doc/format :markdown}
